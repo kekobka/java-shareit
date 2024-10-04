8e85b75c0644fa0ff43f5e0d73412e91a3b49fdf
Valera <108022376+kekobka@users.noreply.github.com>
2024-10-04 20:08:44 +0700

    remove persist

--- server/src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ server/src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -1,29 +1,15 @@
 package ru.practicum.shareit.booking.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import ru.practicum.shareit.booking.BookingStatus;
-import ru.practicum.shareit.exception.BadDateException;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 
 @Data
 @Entity
@@ -52,26 +38,4 @@ public class Booking {
 
     @Enumerated(EnumType.STRING)
     private BookingStatus status;
-
-    @PrePersist
-    @PreUpdate
-    private void validateDates() {
-        LocalDateTime now = LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault());
-
-        if (start == null || end == null) {
-            throw new BadDateException("Дата не может быть пустой");
-        }
-
-        if (start.isBefore(now)) {
-            throw new BadDateException("Время начала не должно быть в прошлом");
-        }
-
-        if (end.isBefore(now)) {
-            throw new BadDateException("Время окончания не должно быть в прошлом");
-        }
-
-        if (!start.isBefore(end)) {
-            throw new BadDateException("Дата начала бронирования должна быть до даты окончания бронирования!");
-        }
-    }
 }
